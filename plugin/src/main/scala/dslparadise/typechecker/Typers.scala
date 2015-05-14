ae2df6bc73ede204e2954fa32eac79a68d4ac9ad
Pascal Weisenburger <pascal.weisenburger@web.de>
2015-05-14 12:47:08 +0200

    only use type aliases to prevent runtime overhead

--- plugin/src/main/scala/dslparadise/typechecker/Typers.scala
+++ plugin/src/main/scala/dslparadise/typechecker/Typers.scala
@@ -1,6 +1,7 @@
 package dslparadise
 package typechecker
 
+import scala.reflect.NameTransformer
 import scala.reflect.internal.Mode
 import dslparadise._
 
@@ -11,32 +12,36 @@ trait Typers {
 
   trait ParadiseTyper extends Typer with TyperContextErrors {
     override def typedArg(arg: Tree, mode: Mode, newmode: Mode, pt: Type): Tree = {
-      if (pt != WildcardType && pt <:< typeOf[ImplicitFunction1[_, _]]) {
-        val convertArg = context inSilentMode {
-          super.typedArg(arg.duplicate, mode, newmode, pt)
-          context.reporter.hasErrors
-        }
+//      val pre = typeOf[dslparadise.`package`.type]
 
-        val newarg = if (convertArg) {
-          val newarg =
-            q"""_root_.dslparadise.ImplicitFunction1 {
-              implicit! => $arg
-            }"""
+      pt match {
+//        case TypeRef(`pre`, sym, _) if sym.name.decodedName.toString == "implicit =>" =>
+        case TypeRef(_, sym, _)
+          if NameTransformer.decode(sym.fullName) == "dslparadise.implicit =>" =>
 
-          val keepArg = context inSilentMode {
-            super.typedArg(newarg.duplicate, mode, newmode, pt)
-            context.reporter.errors exists { _.errPos == NoPosition }
+          val convertArg = context inSilentMode {
+            super.typedArg(arg.duplicate, mode, newmode, pt)
+            context.reporter.hasErrors
           }
 
-          if (keepArg) arg else newarg
-        }
-        else
-          arg
+          val newarg = if (convertArg) {
+            val newarg = q"{ implicit! => $arg }"
 
-        super.typedArg(newarg, mode, newmode, pt)
+            val keepArg = context inSilentMode {
+              super.typedArg(newarg.duplicate, mode, newmode, pt)
+              context.reporter.errors exists { _.errPos == NoPosition }
+            }
+
+            if (keepArg) arg else newarg
+          }
+          else
+            arg
+
+          super.typedArg(newarg, mode, newmode, pt)
+
+        case _ =>
+          super.typedArg(arg, mode, newmode, pt)
       }
-      else
-        super.typedArg(arg, mode, newmode, pt)
     }
   }
 }
